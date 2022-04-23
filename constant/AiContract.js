87ef0b956d8d0288dea4159129a0d2321c0d7a79
Heum-hub <hyeonheum@naver.com>
2022-04-23 11:37:36 +0900

    [feat] constant 필드 파일 생성

---
+++ constant/AiContract.js
@@ -0,0 +1,10 @@
+export const AiContractAbi = [
+    'function swapEthToModel() external payable lock',
+    'function swapModelToEth(uint256 modelTokenAmount) external lock',
+    'function getModelTokenPrice() public view returns (uint256 modelTokenPrice)',
+    'function withdrawLiqutidity() external'
+  ];
+
+export const AiContractAddress = ['0x0000000000000000000000000000000000000000'];
+
+export const ByteCode = "";
---
+++ constant/ERC20Abi.js
@@ -0,0 +1,5 @@
+export const ERC20Abi = [
+    'function allowance(address owner, address spender) public view virtual override returns (uint256)',
+    'function approve(address spender, uint256 amount) public virtual override returns (bool)',
+    'function transfer(address recipient, uint256 amount) public virtual override returns (bool)'
+  ]
