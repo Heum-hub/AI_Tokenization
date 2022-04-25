540d0be9be586fe712408467214fad0e83e63d5e
Heum-hub <hyeonheum@naver.com>
2022-04-25 22:05:29 +0900

    [feat] ContractCall 클래스화, 전역 변수 지정, 데이터 분석 실행 코드 추가

--- ContractCall.tsx
+++ ContractCall.tsx
@@ -4,70 +4,86 @@ import { tokenAddress } from './constant/tokenAddress'
 import { AiContractAddress, AiContractAbi, ByteCode } from './constant/AiContract'
 
 
-// AiModelToken 발행하는 함수
-export async function deployAiContract(privateKey : string, tokenName : string, 
-  tokenSymbol : string, tokenSupply : number) {
-  
+export class ContractCall {
+
+  private privateKey : string;
+
   // Rpc 노드 연결
-  const provider = new ethers.providers.JsonRpcProvider("ethereumRpcUrl");
+  static provider = new ethers.providers.JsonRpcProvider("ethereumRpcUrl");
   // 클라이언트 지갑 연결
-  const signer = new ethers.Wallet(privateKey, provider);
-  // 배포할 aiContractFactory 셍성
-  const aiContractFactory = new ethers.ContractFactory(AiContractAbi, ByteCode, signer);
+  static signer;
 
-  // 컨트랙트에 파라미터 주입
-  const aiContract = await aiContractFactory.deploy( 
-    tokenName, tokenSymbol, tokenSupply);
+  constructor (privateKey: string) {
+
+      this.privateKey = privateKey
+      ContractCall.signer = new ethers.Wallet(this.privateKey, ContractCall.provider);
+
+  }
+
+  // AiModelToken 발행하는 함수
+  async deployAiContract(tokenName : string, 
+      tokenSymbol : string, tokenSupply : number) {
   
-  // 컨트랙트 주소 추가
-  const aiContractAddress = aiContract.address;
-  AiContractAddress.push(aiContractAddress);
+      // 배포할 aiContractFactory 셍성
+      const aiContractFactory = new ethers.ContractFactory(AiContractAbi, ByteCode, ContractCall.signer);
 
-  // 배포 및 로깅
-  console.log(await aiContract.deployTransaction.wait());
-
-}
-
-// AiModel 활용한 분석, 학습 및 이를 위한 클라이언트의 토큰 지불하는 함수
-export async function transactionForAi(privateKey : string, dataUrl : string, aiModelUrl : string, etherAmount : number) {
+      // 컨트랙트에 파라미터 주입
+      const aiContract = await aiContractFactory.deploy( 
+      tokenName, tokenSymbol, tokenSupply);
   
-  // Rpc 노드 연결
-  const provider = new ethers.providers.JsonRpcProvider("ethereumRpcUrl");
-  // 클라이언트 지갑 연결
-  const signer = new ethers.Wallet(privateKey, provider);
+      // 컨트랙트 주소 추가
+      const aiContractAddress = aiContract.address;
+      AiContractAddress.push(aiContractAddress);
 
-  // Ai contract
-  const aiContract = new ethers.Contract(AiContractAddress[0], AiContractAbi, signer);
-  // Ai model token contract
-  const erc20TokenContract = new ethers.Contract(tokenAddress[0], ERC20Abi, signer);
-  
-  // swap Eth for Model Token
-  await aiContract.swapEthToModel({value: ethers.utils.parseEther(etherAmount)});
-  
-  /*
-    데이터 분석 시작
-    1. dataUrl에서 읽어온 데이터 전처리
-    2. aiModelUrl에서 읽어온 모델로 분석 및 학습
-    3. 결과 반환
-  */
+      // 배포 및 로깅
+      console.log(await aiContract.deployTransaction.wait());
 
-  // 결과 정확도 블록체인 상에 기록
-  const accuracy = " ";
+  }
 
-  // send model token to developer
-  // allowance max_num
-  const maxNum = ethers.constants.MaxUint256;
-  // check allowance
-  const allowance = await erc20TokenContract
-  .allowance(signer, AiContractAddress[0]);
-  // if allowance === 0, approve
-  allowance.toString() === '0' ? erc20TokenContract.approve(AiContractAddress[0], maxNum) : ' ';
-  // amount to pay
-  const fromAmount = ethers.utils.parseEther(/*dataUrl에서 읽어온 파일 크기*/);
-  await erc20TokenContract
-  .transfer({data: ethers.utils.parseEther(accuracy)}, 
-  aiContract.getDeveloper(), fromAmount);
-  
+  // AiModel 활용한 분석, 학습 및 이를 위한 클라이언트의 토큰 지불하는 함수
+  async transactionForAi(dataUrl : string, 
+    aiModelUrl : string, etherAmount : number) {
 
+      // Ai contract
+      const aiContract = new ethers.Contract(AiContractAddress[0], AiContractAbi, ContractCall.signer);
+      // Ai model token contract
+      const erc20TokenContract = new ethers.Contract(tokenAddress[0], ERC20Abi, ContractCall.signer);
   
+      // swap Eth for Model Token
+      await aiContract.swapEthToModel({value: ethers.utils.parseEther(etherAmount)});
+  
+      // 데이터 분석 프로세스 시작
+      let fileSize : number;
+      let accuracy : number;
+      
+      //  1. dataUrl에서 읽어온 데이터 크기 확인
+      const fs  = require('fs');
+      fs.readFile(dataUrl, (err, data) => {
+        fileSize =  data.toString().length
+      })
+
+      // 2. aiModelUrl에 있는 모델에 데이터 url 전달하여 실행(데이터 전처리, 분석 및 학습)
+      const spawn = require('child_process').spawn;
+      const result = spawn('python', ['aiModelUrl', 'dataUrl'])
+      // 3. 결과 반환(결과 정확도 블록체인 상에 기록 위함)
+      result.stdout.on('data', (data) => {  accuracy = data.toString().parseInt() });
+      // 데이터 분석 종료
+
+
+      // send model token to developer
+      // allowance max_num
+      const maxNum = ethers.constants.MaxUint256;
+      // check allowance
+      const allowance = await erc20TokenContract
+      .allowance(ContractCall.signer, AiContractAddress[0]);
+      // if allowance === 0, approve
+      allowance.toString() === '0' ? erc20TokenContract.approve(AiContractAddress[0], maxNum) : ' ';
+      // amount to pay
+      const fromAmount = ethers.utils.parseEther(fileSize);
+      await erc20TokenContract
+      .transfer({data: ethers.utils.parseEther(accuracy)}, 
+      aiContract.getDeveloper(), fromAmount);
+  
+  }
+
 }
